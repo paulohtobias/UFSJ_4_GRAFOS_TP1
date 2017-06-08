433560f9f169dd71c167b09240d36bcd82563816
Millas <millas5444@gmail.com>
2017-06-08 13:53:09 -0300

    Definiu o gsudoku como global

--- include/interface.h
+++ include/interface.h
@@ -13,7 +13,7 @@ typedef struct{
 	GtkWidget ***button;
 }gui_sudoku;
 //Cria um grid.
-GtkWidget *gui_cria_grid(gui_sudoku *sudoku_gui, int *ids, int *cor);
+GtkWidget *gui_cria_grid( int *ids, int *cor);
 //Cria o Sudoku.
 gui_sudoku* gui_cria_sudoku(Sudoku *sudoku);
 
@@ -23,10 +23,9 @@ typedef struct{
 	int linha;
 	int coluna;
 	int cor;
-	gui_sudoku *gsudoku;
 }gui_colore_vertice_dados;
 //Inicializa os dados que serão passados para a função gui_colore_vertice
-gui_colore_vertice_dados *gui_colore_dados_novo(gui_sudoku *gsudoku, int id, int linha, int coluna, int cor);
+gui_colore_vertice_dados *gui_colore_dados_novo( int id, int linha, int coluna, int cor);
 //Muda o label do botão se foi possível mudar sua cor.
 bool gui_colore_vertice(GtkButton *vertice, gpointer data);
 
--- src/interface.c
+++ src/interface.c
@@ -1,14 +1,16 @@
 #include <gtk/gtk.h>
 #include "interface.h"
 
+gui_sudoku *gsudoku;
+
 ///Principal
 //Cria um grid.
-GtkWidget *gui_cria_grid(gui_sudoku *sudoku_gui, int *ids, int *cor){
+GtkWidget *gui_cria_grid(int *ids, int *cor){
 	int i,j;
 
-	int dimensao = sudoku_gui->sudoku->dimensao;
-	int altura = sudoku_gui->sudoku->altura;
-	int largura = sudoku_gui->sudoku->largura;
+	int dimensao = gsudoku->sudoku->dimensao;
+	int altura = gsudoku->sudoku->altura;
+	int largura = gsudoku->sudoku->largura;
 
 	//Inicialização
 	GtkWidget *vertical = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
@@ -33,8 +35,8 @@ GtkWidget *gui_cria_grid(gui_sudoku *sudoku_gui, int *ids, int *cor){
 			if(cor[ids[k]] == 0){
 				strcpy(label, " ");
 			}
-			sudoku_gui->button[linha][coluna] = gtk_button_new_with_label(label);
-			gtk_box_pack_start(GTK_BOX(horizontal[i]), sudoku_gui->button[linha][coluna], TRUE, TRUE, 0);
+			gsudoku->button[linha][coluna] = gtk_button_new_with_label(label);
+			gtk_box_pack_start(GTK_BOX(horizontal[i]), gsudoku->button[linha][coluna], TRUE, TRUE, 0);
 		}
 	}
 	return vertical;
@@ -54,40 +56,39 @@ gui_sudoku* gui_cria_sudoku(Sudoku *sudoku){
 	colunas= dimensao/largura;
 	
 	///Inicialização
-	gui_sudoku *sudoku_gui = malloc(sizeof(gui_sudoku));
+	gsudoku = malloc(sizeof(gui_sudoku));
 
-	sudoku_gui->sudoku = sudoku;
+	gsudoku->sudoku = sudoku;
 
-	sudoku_gui->box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
+	gsudoku->box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
 
 	GtkWidget *linha[linhas];
 	GtkWidget *grid[linhas][colunas];
 	
-	sudoku_gui->button = malloc(dimensao * sizeof(GtkWidget**));
+	gsudoku->button = malloc(dimensao * sizeof(GtkWidget**));
 	for(i=0; i<dimensao; i++){
-		sudoku_gui->button[i] = malloc(dimensao * sizeof(GtkWidget*));
+		gsudoku->button[i] = malloc(dimensao * sizeof(GtkWidget*));
 	}
 
 	for(i = 0; i < linhas; i++){
 		linha[i] = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 5);
 		gtk_box_set_homogeneous(GTK_BOX(linha[i]), TRUE);
-		gtk_box_pack_start(GTK_BOX(sudoku_gui->box), linha[i], TRUE, TRUE, 0);
+		gtk_box_pack_start(GTK_BOX(gsudoku->box), linha[i], TRUE, TRUE, 0);
 
 		for(j=0; j < colunas; j++){
-			grid[i][j] = gui_cria_grid(sudoku_gui, sudoku_ids_quadrante(altura, largura, j, i), sudoku->grafo->cor);
+			grid[i][j] = gui_cria_grid( sudoku_ids_quadrante(altura, largura, j, i), sudoku->grafo->cor);
 			gtk_box_pack_start(GTK_BOX(linha[i]), grid[i][j], TRUE, TRUE, 0);
 		}
 	}
 	
-	return sudoku_gui;
+	return gsudoku;
 }
 
 ///Coloração
 //Inicializa os dados que serão passados para a função gui_colore_vertice
-gui_colore_vertice_dados *gui_colore_dados_novo(gui_sudoku *gsudoku, int id, int linha, int coluna, int cor){
+gui_colore_vertice_dados *gui_colore_dados_novo(int id, int linha, int coluna, int cor){
 	gui_colore_vertice_dados *dados = malloc(sizeof(gui_colore_vertice_dados));
 
-	dados->gsudoku = gsudoku;
 	dados->id = id;
 	dados->linha = linha;
 	dados->coluna = coluna;
@@ -100,21 +101,21 @@ bool gui_colore_vertice(GtkButton *vertice, gpointer data){
 	gui_colore_vertice_dados *dados = (gui_colore_vertice_dados*)data;
 
 	int id = dados->id;
-	int dimensao = dados->gsudoku->sudoku->dimensao;
+	int dimensao = gsudoku->sudoku->dimensao;
 
 	if(dados->id < 0){
 		//Se o id não estiver definido, então foi usado a posição linha x coluna do Sudoku.
 		id = sudoku_lc_para_vertice_id(dimensao, dados->linha, dados->coluna);
 	}
 
-	if(grafo_colore_vertice(dados->gsudoku->sudoku->grafo, id, dados->cor) == true){
+	if(grafo_colore_vertice(gsudoku->sudoku->grafo, id, dados->cor) == true){
 		int *sudoku_lc = vertice_id_para_sudoku_lc(dimensao, id);
 		int linha = sudoku_lc[0];
 		int coluna = sudoku_lc[1];
 		
 		char label[10];
 		sprintf(label, "%d", dados->cor);
-		gtk_button_set_label(GTK_BUTTON(dados->gsudoku->button[linha][coluna]), label);
+		gtk_button_set_label(GTK_BUTTON(gsudoku->button[linha][coluna]), label);
 
 		return true;
 	}
@@ -148,9 +149,9 @@ int gui(int argc, char *argv[]){
 
 	GtkWidget *lado_esquerdo = GTK_WIDGET(gtk_builder_get_object(gtkBuilder,"lado_esquerdo"));
 	
-	gui_sudoku *sudoku_gui = gui_cria_sudoku(sudoku);
+	gsudoku = gui_cria_sudoku(sudoku);
 
-	gtk_box_pack_start(GTK_BOX(lado_esquerdo), sudoku_gui->box, TRUE, TRUE, 0);
+	gtk_box_pack_start(GTK_BOX(lado_esquerdo), gsudoku->box, TRUE, TRUE, 0);
 
 	gtk_widget_show_all(janela);
 
@@ -158,13 +159,14 @@ int gui(int argc, char *argv[]){
 	printf("colore: %d\n",
 		gui_colore_vertice(
 			NULL,
-			gui_colore_dados_novo(sudoku_gui, -1, 7, 5, 9)
+			gui_colore_dados_novo(-1, 7, 5, 9)
 		)
 	);
+
 	printf("colore: %d\n",
 		gui_colore_vertice(
 			NULL,
-			gui_colore_dados_novo(sudoku_gui, -1, 7, 2, 9)
+			gui_colore_dados_novo(-1, 7, 2, 9)
 		)
 	);
 
